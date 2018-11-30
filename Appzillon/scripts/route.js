eced7a4171c1455aeadd1b7ea90b21ba4a77c67b
prudhvi.kumar <prudhvi.kumar@i-exceed.com>
2018-11-30 19:01:29 +0530

    All

---
+++ Appzillon/scripts/route.js
@@ -0,0 +1,11 @@
+function macos() {
+    window.location = "http://192.168.1.6/Appzillon.zip";
+}
+
+function windows() {
+    window.location = "http://192.168.1.6/Appzillon.exe";
+}
+
+function linux() {
+    window.location = "http://192.168.1.6/appzillon.deb";
+}
